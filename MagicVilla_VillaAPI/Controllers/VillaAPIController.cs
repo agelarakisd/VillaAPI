36bee559bf7bd083b5c596c12c988d3544d4443c
james who <darklordx@gmail.com>
2023-10-27 16:49:01 +0300

    tt

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -83,6 +83,8 @@ public class VillaAPIController : ControllerBase
     }
 
     [HttpPut("{id:int}", Name = "UpdateVilla")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public IActionResult UpdateVilla(int id, [FromBody] VillaDTO villaDTO)
     {
         if (villaDTO == null || id != villaDTO.Id)
